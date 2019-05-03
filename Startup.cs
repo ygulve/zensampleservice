558ef4782590dd33a233e3cfd0b827ed746b5217
ygulve <38343976+ygulve@users.noreply.github.com>
2019-05-03 16:56:26 +0530

    Service files

---
+++ Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ZenDerivco.Data;
+using ZenDerivco.Models;
+using ZenDerivco.Models.Repository;
+using ZenDerivco.Models.Repositroy;
+
+namespace ZenDerivco
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {          
+            services.AddCors();
+
+            services.AddDbContext<ZenDerivcoContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:ZenDerivco"]));
+            services.AddScoped<IDataRepository<Employee>, EmployeeManager>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                        .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+         
+            services.AddMvc().AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddCors(o => o.AddPolicy("AllowSpecificOrigin", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithOrigins("http://localhost:4200");
+                //.WithOrigins("http://localhost:4200", "http://10.120.87.188:8080");
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors("AllowSpecificOrigin");
+            app.UseMvc();
+        }
+    }
+}
---
+++ ZenDerivcoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenDerivco.Models;
+
+namespace ZenDerivco
+{
+    public class ZenDerivcoContext:DbContext
+    {
+
+        public ZenDerivcoContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
